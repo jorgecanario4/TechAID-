4d0766c810edaa28d79a6d4a021a4b6da9a7acb9
jorgecanario4 <jorgeic.767@gmail.com>
2018-04-16 13:30:29 -0400

    Update jwptest.py

--- jwptest.py
+++ jwptest.py
@@ -9,10 +9,22 @@ def driver():
     url="https://www.univision.com/los-angeles/kmex/noticias/asesinatos/en-video-abandonan-el-cadaver-de-un-pandillero-junto-a-un-contenedor-de-basura"
     driver = webdriver.Firefox()
     driver.get(url)
-    return driver
+    yield driver
+    driver.close()
 
-def is_Paused(driver):
-    driver.execute_script("window.scrollTo(0, 300)")
+def test_playPause(driver):
+    vidElement = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//video[@class='jw-video jw-reset']")))
+
+    vidElement.click()
+
+    driver.implicitly_wait(100)
+
+    vidElement.click()
+    driver.implicitly_wait(100)
+    assert 1 == 1
+    
+def test_is_Paused(driver):
+    driver.execute_script("window.scrollTo(0, 250)")
     vidElement = WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, "//video[@class='jw-video jw-reset']")))
     #//div[@class='jw-icon jw-icon-inline jw-button-color jw-reset jw-icon-playback']
     vidElement.click()
@@ -27,15 +39,5 @@ def is_Paused(driver):
 
     durationTime= driver.find_elements(By.XPATH, "//div[@class='jw-icon jw-icon-inline jw-text jw-reset jw-text-duration']")
 
-    print "Video Duration Time is:" + durationTime
+    print ("Video Duration Time is:" + durationTime)
     assert timeBeforePause == timeAfterPause
-
-def playPause(driver):
-    vidElement = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//video[@class='jw-video jw-reset']")))
-
-    vidElement.click()
-
-    driver.implicitly_wait(5)
-
-    vidElement.click()
-    assert 1 == 1
